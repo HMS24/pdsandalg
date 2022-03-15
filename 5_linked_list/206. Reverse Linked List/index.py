b1e1ef6f29b3c3584dac5c82cebec8943628d69f
chenrohua24 <z7707092003@gmail.com>
2022-03-15 11:52:58 +0800

    206 approach

--- 5_linked_list/206. Reverse Linked List/index.py
+++ 5_linked_list/206. Reverse Linked List/index.py
@@ -30,3 +30,61 @@ class Solution_1:
             next_n.next = cur_n
 
         return head
+
+
+# Approach 2 Iterative / Swapping
+"""
+ListNode{val: 1, 
+    next: ListNode{val: 2, 
+        next: ListNode{val: 3, 
+            next: ListNode{val: 4, 
+                next: ListNode{val: 5, 
+                    next: None}}}}}
+
+1. head node 存在？ current = head
+2. 存下 current.next
+3. current.next 指向 prev=None
+4. prev = current
+5. current = next
+"""
+
+class Solution_2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:        
+        prev = None
+        current = head
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+
+        return prev
+
+
+"""
+印出 ListNodes
+"""
+class ListNode:
+    def __init__(self, val):
+        self.next = None
+        self.val = val
+
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+e = ListNode(5)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+def printList(cur: ListNode):
+    if cur:
+        print(cur.val, " -> ", end='')
+        printList(cur.next)
+    else:
+        print("None")
+        
+printList(a)
