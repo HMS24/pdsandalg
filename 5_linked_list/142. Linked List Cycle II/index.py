030130b8915f4f3ef05523be5febf87a2be36f0d
chenrohua24 <z7707092003@gmail.com>
2022-03-23 10:40:21 +0800

    142 approach 2 - slow fast pointer

--- 5_linked_list/142. Linked List Cycle II/index.py
+++ 5_linked_list/142. Linked List Cycle II/index.py
@@ -40,12 +40,50 @@ class Solution_1:
 
 # Approach1 slow fast pointer
 """
+https://leetcode.com/problems/linked-list-cycle-ii/discuss/1701128/C%2B%2BJavaPython-Slow-and-Fast-oror-Image-Explanation-oror-Beginner-Friendly
+
+Floyd Cycle Algorithm
+
+* 點
+    起點 s 
+    循環進入點 e 
+    slow 及 fast 交會點 x
+* 距離
+    s 到 e 的距離 H
+    e 到 x 的距離 D
+    一圈的距離 L
+* 演算
+    因為 fast 的速率是 slow 的 2 倍
+    所以
+    slow 走 H+D
+    fast 走 2H+2D
+    又
+    fast 實際上走 H+D+nL
+    所以
+    2H+2D = H+D+nL
+    => H = (L-D) + (n-1)L
+
+1. 先找到 slow 及 fast 的交會點
+2. 重複 1 直到 slow == fast 否則 None
+3. 再從交會點及從 head 設另一個指針，繼續走, 因為H = (L-D) + (n-1)L
+4. 新交會點就是循環進入點
 
 """
 
 class Solution_2:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:        
-        pass
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        
+        while head != slow:
+            head, slow = head.next, slow.next
+
+        return head
 
 
 
