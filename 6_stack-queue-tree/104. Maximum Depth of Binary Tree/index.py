e4114c770a9a071afe7e0efe20f2a51b32435a18
chenrohua24 <z7707092003@gmail.com>
2022-04-29 12:54:39 +0800

    Fix file

--- 6_stack-queue-tree/104. Maximum Depth of Binary Tree/index.py
+++ 6_stack-queue-tree/104. Maximum Depth of Binary Tree/index.py
@@ -1,4 +1,4 @@
-"""https://leetcode.com/problems/minimum-depth-of-binary-tree/
+"""https://leetcode.com/problems/maximum-depth-of-binary-tree/
 """
 
 from typing import Optional
@@ -11,41 +11,20 @@ class TreeNode:
         self.right = right
 
 
-# Approach 1 Recursion 深度優先
+# Approach1 Recursion
 """
 如果有左子樹，則遞迴算出左子樹高度
 如果有右子樹，則遞迴算出右子樹高度
-如果只有左子樹，回傳右邊高度
-如果只有右子樹，回傳左邊高度
-因為是算跟 leaf node 的高度，根不算
+回傳左右子樹高度最大者
 """
 
 class Solution_1:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         def get_height(node, h=1):
-            if node is None:
-                return 0
             left_h = get_height(node.left, h+1) if node.left else h
             right_h = get_height(node.right, h+1) if node.right else h
+            return max(left_h, right_h)
 
-            if node.left is None and node.right:
-                return right_h
-            elif node.right is None and node.left:
-                return left_h
-            else:
-                return min(left_h, right_h)
-
-        return get_height(root)
-
-# Approach 2 Recursion 深度優先 優化！
-"""
-"""
-
-class Solution_2:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
-                return 0
-        if None in [root.left, root.right]:
-            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
-        else:
-            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+            return 0
+        return get_height(root)
