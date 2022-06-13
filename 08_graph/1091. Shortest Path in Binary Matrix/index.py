9f4741d499279d857bf5b9f8bb0033be49d428a2
chenrohua24 <z7707092003@gmail.com>
2022-06-13 16:38:20 +0800

    1091. Shortest Path in Binary Matrix 2

--- 08_graph/1091. Shortest Path in Binary Matrix/index.py
+++ 08_graph/1091. Shortest Path in Binary Matrix/index.py
@@ -46,3 +46,38 @@ class Solution_1:
         visited[0][0] = True
         walk(0, 0)
         return min(minimums) if minimums else -1
+
+# Approach 2 bfs
+class Solution_2:
+    def shortestPathBinaryMatrix(self, grid):
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
+            return -1
+        n = len(grid)
+        for i in range(n):
+            if grid[i][i] == 1:
+                break
+        else:
+            return n
+        
+        steps = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+        visited = set()
+        pathes = [(0, 0, 1)]
+        visited.add((0, 0))
+        
+        while pathes:
+            row, column, shortest_path = pathes.pop(0)
+            if (row, column) == (n-1, n-1):
+                return shortest_path
+
+            for step in steps:
+                x = row + step[0]
+                y = column + step[1]
+                if x not in range(n) or y not in range(n):
+                    continue
+                if grid[x][y] == 1:
+                    continue
+                if (x, y) in visited:
+                    continue
+                visited.add((x, y))
+                pathes.append((x, y, shortest_path+1))
+        return -1
