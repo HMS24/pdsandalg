ce5ad89e677249804b599a517cf5f5dcc9f25d37
chenrohua24 <z7707092003@gmail.com>
2022-06-17 10:51:22 +0800

    207. Course Schedule bfs

--- 10_graph/207. Course Schedule/index.py
+++ 10_graph/207. Course Schedule/index.py
@@ -5,6 +5,7 @@
 # Time Limit Exceeded
 class Solution_1:
     def canFinish(self, numCourses, prerequisites):
+        # 利用 dict 紀錄依賴關係
         deps_dict = {}
         for take, need in prerequisites:
             if need in deps_dict:
@@ -12,6 +13,7 @@ class Solution_1:
             else:
                 deps_dict[need] = [take]
         
+        # 輪流從每個節點開始，紀錄走過的 path，每個節點開始就清掉 path
         path = set()
         for target in range(numCourses):
             path.clear()
@@ -25,6 +27,7 @@ class Solution_1:
         if target not in deps_dict:
             return True
             
+        # 輪流加入 dep dfs，但這樣走過的還會再走一次。
         for course in deps_dict[target]:
             path.add(course)
             if not self.dfs(course, deps_dict, path):
@@ -39,7 +42,7 @@ class Solution_2:
         deps_dict = {}
         for target, need in prerequisites:
             if target in deps_dict:
-                deps_dict[target].append(need)
+                deps_dict[target].append(need)ㄍ　「／討
             else:
                 deps_dict[target] = [need]
                 
@@ -58,10 +61,43 @@ class Solution_2:
             return True
         if target in checking:
             return False
-            
+    
         checking.add(target)
         for dep_course in deps_dict[target]:
             if not self.dfs(dep_course, deps_dict, checking, checked):
                 return False
+        # 紀錄已經走進去的點，下次就無需再走一次了。
         checked.add(target)
         return True
+
+# Approach 3 bfs
+class Solution_3:
+    def canFinish(self, numCourses, prerequisites):
+        # deps == dependences, deped == depended
+        deps = [ [] for _ in range(numCourses)]
+        deped_count = [0 for _ in range(numCourses)]
+
+        # 紀錄被依賴的次數 edge
+        for target, need in prerequisites:
+            deps[target].append(need)
+            deped_count[need] += 1
+
+        # 從沒有被依賴的節點開始
+        queue = []
+        for course, count in enumerate(deped_count):
+            if count == 0:
+                queue.append(course)
+        
+        while queue:
+            target = queue.pop(0)
+            for dep in deps[target]:
+                deped_count[dep] -= 1
+                if deped_count[dep] == 0:
+                    queue.append(dep)
+
+        # 有刪不掉的 edge 代表存在 cycle
+        for count in deped_count:
+            if count != 0:
+                return False
+        return True
+            
