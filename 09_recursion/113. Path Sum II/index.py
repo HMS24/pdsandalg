29756ed52ee492369bc49d28804c4ec351f321a8
chenrohua24 <z7707092003@gmail.com>
2022-07-21 12:14:44 +0800

    refactor: 113 dfs and bfs approach

--- 09_recursion/113. Path Sum II/index.py
+++ 09_recursion/113. Path Sum II/index.py
@@ -7,66 +7,101 @@ class TreeNode:
         self.left = left
         self.right = right
 """
-
 # Approach 1 dfs
-class Solution_1:
+
+
+class Solution1:
     def pathSum(self, root, targetSum):
         if not root:
             return []
         results = []
-        self.dfs(root, targetSum, [root.val], 0+root.val, results)
+
+        def dfs(root, path, rest_num):
+            if not root.left and not root.right and rest_num == 0:
+                return results.append(path[:])
+
+            # 參數 copy 一份 list 不斷新增 list，對記憶體負擔較大。
+            if root.left:
+                dfs(root.left, path+[root.left.val], rest_num-root.left.val)
+
+            if root.right:
+                dfs(root.right, path+[root.right.val], rest_num-root.right.val)
+
+        dfs(root, [root.val], targetSum-root.val)
+
         return results
 
-    def dfs(self, root, targetSum, path, pathSum, results):
-        if pathSum == targetSum and not root.left and not root.right:
-            results.append(path[:])
-            return
-
-        # 在參數 copy 一份 list 不斷新增 list，對記憶體負擔較大。
-        if root.left:
-            self.dfs(root.left, targetSum, path+[root.left.val], pathSum+root.left.val, results)
-        if root.right:
-            self.dfs(root.right, targetSum, path+[root.right.val], pathSum+root.right.val, results)
+# Approach 2 dfs improve
 
 
-# Approach 2 iteration using queue
-class Solution_2:
+class Solution2:
     def pathSum(self, root, targetSum):
+        if not root:
+            return []
         results = []
-        queue = []
-        queue.append([root, [root.val], root.val])
+
+        def dfs(root, path, rest_num):
+            if not root:
+                return
+            # notice ! rest_num == root.val
+            if not root.left and not root.right and rest_num == root.val:
+                return results.append(path[:]+[root.val])
+
+            # backtracking
+            path.append(root.val)
+            dfs(root.left, path, rest_num-root.val)
+            dfs(root.right, path, rest_num-root.val)
+            path.pop(-1)
+
+        dfs(root, [], targetSum)
+
+        return results
+
+# Approach 3 iteration using queue
+
+
+class Solution3:
+    def pathSum(self, root, targetSum):
+        if not root:
+            return []
+        results = []
+        queue = [[root, [root.val], targetSum-root.val]]
         while queue:
-            node, path, sum = queue.pop(0)
-            if sum == targetSum and not node.left and not node.right:
+            node, path, rest_num = queue.pop(0)
+            if rest_num == 0 and not node.left and not node.right:
                 results.append(path)
             if node.left:
-                queue.append([node.left, path+[node.left.val], sum+node.left.val])
+                queue.append(
+                    [node.left, path+[node.left.val], rest_num-node.left.val]
+                )
             if node.right:
-                queue.append([node.right, path+[node.right.val], sum+node.right.val])
+                queue.append(
+                    [node.right, path+[node.right.val], rest_num-node.right.val]
+                )
 
         return results
 
+# Approach 4 iteration using queue
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 
-r = TreeNode(
-    5,
-    TreeNode(
-        4,
-        TreeNode(1),
-        TreeNode(9),
-    ),
-    TreeNode(
-        8,
-        TreeNode(2),
-        TreeNode(5),
-    )
-)
+class Solution4:
+    def pathSum(self, root, targetSum):
+        if not root:
+            return []
+        results = []
+        queue = [[root, [root.val], targetSum-root.val]]
+        while queue:
+            for _ in range(len(queue)):
+                node, path, rest_num = queue.pop(0)
+                if rest_num == 0 and not node.left and not node.right:
+                    results.append(path)
+                if node.left:
+                    queue.append(
+                        [node.left, path+[node.left.val], rest_num-node.left.val]
+                    )
+                if node.right:
+                    queue.append(
+                        [node.right, path+[node.right.val], rest_num-node.right.val]
+                    )
 
-s = Solution_2()
-res = s.pathSum(r, 18)
-print(res)
+        return results
