d785c6900a90d3fd84a36d8464b1825c6f4d895a
chenrohua24 <z7707092003@gmail.com>
2022-07-22 10:48:52 +0800

    refactor: add comment

--- 09_recursion/77. Combinations/index.py
+++ 09_recursion/77. Combinations/index.py
@@ -2,30 +2,39 @@
 """
 
 # Approach 1 backtracking
-class Solution_1:
+
+
+class Solution1:
     def combine(self, n, k):
         results = []
-        self.backtracking(1, n, k, [], results)
+
+        def backtracking(start, curr):
+            if len(curr) == k:
+                return results.append(curr[:])
+            for i in range(start, n+1):
+                curr.append(i)
+                backtracking(i+1, curr)
+                curr.pop()
+
+        backtracking(1, [])
+
         return results
 
-    def backtracking(self, first, n, k, curr, results):
-        if len(curr) == k:
-            results.append(curr[:])
-            return
-        for i in range(first, n+1):
-            curr.append(i)
-            self.backtracking(i+1, n, k, curr, results)
-            curr.pop()
-
 # Approach 2 組合公式
-class Solution_2:
+
+
+class Solution2:
     def combine(self, n, k):
+        # e.g. c(4, 1) = [[1], [2], [3], [4]]
         if k == 1:
             return [[i] for i in range(1, n+1)]
+         # e.g. c(4, 4) = [[1, 2, 3, 4]]
         if k == n:
             return [[*range(1, n+1)]]
+
         results1 = self.combine(n-1, k-1)
+        # 一定要取第 1 個數字也就是 n
         results1 = [item+[n] for item in results1]
+
         results2 = self.combine(n-1, k)
         return results1 + results2
-        
