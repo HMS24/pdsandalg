02a514e0b81da2b25fbb1f683a7586ab0d7b5cec
chenrohua24 <z7707092003@gmail.com>
2022-08-23 10:58:16 +0800

    chore: 91 note

--- 11_dp/91. Decode Ways/index.py
+++ 11_dp/91. Decode Ways/index.py
@@ -5,19 +5,40 @@
 class Solution1:
     def decodeWays(self, s):
         """
-        :type s: str
-        :rtype: int
+            223
+            可以 decompose 
+
+            - level 1
+                1 character
+                    2 23
+                2 character
+                    22 3
+
+            level 2
+                1 character
+                    2 2 3 
+                    pointer at 3
+                2 character
+                    2 23
+                    pointer at empty string 超過 str 的 len so return 1
+
+            level 3
+                1 character
+                    2 2 3
+                    pointer at empty string return 1
         """
         n = len(s)
         cache = [-1] * n
 
         def num_decodings(pointer):
+            # base case 超過 str len
             if pointer >= n:
                 return 1
             if cache[pointer] > -1:
                 return cache[pointer]
 
             total_decompositions = 0
+            # 可以 decompose 1 or 2 characters
             for i in range(1, 2+1):
                 if pointer + i > n:
                     break
