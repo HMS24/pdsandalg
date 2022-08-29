ba7df2dabd0b21014c68fbc9d2aec251c175ab4f
chenrohua24 <z7707092003@gmail.com>
2022-08-29 11:27:28 +0800

    feat: sort 9998

---
+++ 12_sort/9998. Mergesort/index.py
@@ -0,0 +1,65 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+"""
+Mergesort
+
+"""
+
+
+class Solution:
+    def mergeSort(self, head):
+        '''
+        :type head: ListNode
+        :rtype: ListNode
+        '''
+        # split
+        #     - get_middle_node
+        #     - left = mergeSort(head)
+        #     - right = mergeSort(mid.next)
+        # merge
+        #     - compare left right
+        return self.split(head)
+
+    def split(self, head):
+        # base case
+        if head is None or head.next is None:
+            return head
+
+        mid_node = self._get_middle_node(head)
+        right_head = mid_node.next
+        mid_node.next = None
+
+        left = self.split(head)
+        right = self.split(right_head)
+
+        return self.merge(left, right)
+
+    @staticmethod
+    def _get_middle_node(head):
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+    def merge(self, left, right):
+        start = ListNode()
+        head = start
+
+        while left and right:
+            if left.val < right.val:
+                start.next = left
+                left = left.next
+            else:
+                start.next = right
+                right = right.next
+            start = start.next
+
+        start.next = left or right
+
+        return head.next
